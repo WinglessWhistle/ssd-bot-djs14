c711eb52aa711f151a18827db716a3e2768b542b
45ninjas <tom.parker1235@gmail.com>
2022-01-11 17:18:42 +0800

    Made help cmd work
    Signed-off-by: 45ninjas <tom.parker1235@gmail.com>

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,19 @@
 module.exports = {
     name: 'help',
     description: "displays this message",
-    execute(message, args){
-        message.channel.send("Hello, This IS the help message.");
+    execute(context, message, args) {
+
+        // make some variable names a little shorter.
+        const commands = context.commands.List;
+        helptext = "";
+
+        // Go over each command say something about each command.
+        commands.forEach((value, key, commands) => {
+            helptext += context.prefix;
+            helptext += key.padEnd(12, ' ');
+            helptext += value.description;
+            helptext += '\n';
+        });
+        message.channel.send("```" + helptext + "```");
     }
 }
