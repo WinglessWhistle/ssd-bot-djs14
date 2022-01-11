e5c531057a55c9ff97bced1fb164172f03d6598e
45ninjas <tom.parker1235@gmail.com>
2022-01-12 01:54:25 +0800

    Added an info command
    Signed-off-by: 45ninjas <tom.parker1235@gmail.com>

---
+++ commands/info.js
@@ -0,0 +1,25 @@
+const { exec } = require("child_process");
+const { MessageEmbed } = require("discord.js");
+const { hostname } = require("os");
+const { init } = require("./miitopia");
+gitDetails = null;
+    module.exports = {
+        name: 'info',
+        description: "displays info for this bot.",
+        execute(context, message, args) {
+            const embed = new MessageEmbed()
+                .setColor("GREEN")
+                .setTitle("info")
+                .setDescription(`Latest commit hash: ${gitDetails[0]}
+                Latest commit date: ${gitDetails[1]}
+                ${gitDetails[2]},${gitDetails[3]}
+
+                Current Hostname: ${hostname}`);
+            message.channel.send({ embeds: [embed] });
+        },
+        init() {
+            exec('git log -n 1 --pretty=format:"%h,%cs,%d"', (error, stdout, stderr) => {
+                gitDetails = stdout.split(',');
+            });
+        }
+    }
